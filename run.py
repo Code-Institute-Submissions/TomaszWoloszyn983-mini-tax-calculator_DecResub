f5a652cfdc17948813f8d26815dbcfd5e5c3d08a
Tomasz <tomaszwoloszyn983@gmail.com>
2022-07-05 07:03:09 +0000

    Add welcome message and get_user_name function

--- run.py
+++ run.py
@@ -18,3 +18,25 @@ data = info.get_all_values()
 
 print(data)
 
+def welcome_message():
+    """
+    Dispay greetings, information about the application
+    and instruction for the user.
+    """
+    print("Welcome to Mini Tax Calculator.\n")
+    print("Thank You for using our application.")
+    print("This application will help You qickly calculate your taxes\n")
+    print("The application needs to ask you for few informations that are necessary for calculate your taxes\n")
+    print("All sensivite data are to be used for the calculations puposes only, and will never be shared")
+    print("or used for any other purpose.")
+    print(f"Welcome {get_user_name()} Thank You for using our application")
+    
+
+def get_user_name():
+    """
+    Display asking for the users name.
+    Returns that name.
+    """
+    return input("Enter your data here: ")
+
+welcome_message()
