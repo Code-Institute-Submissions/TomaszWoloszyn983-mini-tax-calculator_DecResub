0ec8a27a3fe09814ef676e9686fb9c676ffd7909
Tomasz <tomaszwoloszyn983@gmail.com>
2022-07-19 22:09:08 +0000

    Pass code validation test. Error in displaying results

--- run.py
+++ run.py
@@ -24,12 +24,14 @@ data = info.get_all_values()
 # prints data from the google sheets
 # pprint(data)
 
+
 def update_sheet(person):
     """
     Creates a list of data to be sent to the google sheet.
     The order of the elements is important. It is : Name, Age, Salary, Tax
     """
-    users_data = [person.name, person.age, person.married, person.salary, person.taxes]
+    users_data = [person.name, person.age,
+                  person.married, person.salary, person.taxes]
     print(f"Send {person.name}'s data to the sheet")
     info.append_row(users_data)
     print("Data sent!")
@@ -70,10 +72,11 @@ def get_user_name():
     print(f"Welcome {name} Thank You for using our application")
     return name
 
+
 def validate_name(name):
     """
     Validate input data.
-    Check if the name doesn't contain other than letters. 
+    Check if the name doesn't contain other than letters.
     Accepted are only upper and lowercase letters.
     Also the name should contain at least three letter.
     """
@@ -82,20 +85,24 @@ def validate_name(name):
         return name
 
     else:
-        print ("Name is invalid! The name should contain at least three letters from A to Z or a to z.")
+        print ('Name is invalid! The name should contain at '
+               'least three letters from A to Z or a to z.')
         return validate_name(input("Please enter your name again \n"))
 
 
 def quit_all():
     """
-    This function is called when Q-key is pressed on the keyboard 
-    it interrupts the whole process and 
-    moves the user to the welcome_message function where he can 
+    This function is called when Q-key is pressed on the keyboard
+    it interrupts the whole process and
+    moves the user to the welcome_message function where he can
     start the application from the beginning.
     """
     print("Are you sure you want to quit the process?")
     while True:
-        confirm = input(f'Press {Back.YELLOW}{Fore.BLACK}"Y"{Style.RESET_ALL} to quit or {Back.YELLOW}{Fore.BLACK}"N"{Style.RESET_ALL} to return\n')
+        confirm = input(f'Press "{Back.YELLOW}{Fore.BLACK} Y '
+                        f'{Style.RESET_ALL}" to quit or "'
+                        f'{Back.YELLOW}{Fore.BLACK} N '
+                        f'{Style.RESET_ALL}" to return\n')
         if confirm.upper() == "Y":
             print("Process interrupted by the user\n\n")
             clear()
@@ -109,18 +116,22 @@ def quit_all():
 
 def request_salary():
     """
-    Request salary from the user. 
-    The entered value has to be validated for being integer and 
+    Request salary from the user.
+    The entered value has to be validated for being integer and
     having the right format.
-    If the user doesn't know the value of his salary it can be 
+    If the user doesn't know the value of his salary it can be
     calculated after typing C-key on keyboard.
     """
     while True:
-        print(f'Please enter your weekly {Back.YELLOW}{Fore.BLACK}salary{Style.RESET_ALL} in the following format: 99.99')
-        print(f'or select option {Back.YELLOW}{Fore.BLACK}"C"{Style.RESET_ALL} to calculate it.')
-        user_input = input(f'If you want to quit You can select option {Back.YELLOW}{Fore.BLACK}"Q"{Style.RESET_ALL}.\n')
-        if user_input.upper() == "Q":  
-            if quit_all() == False:
+        print(f'Please enter your weekly {Back.YELLOW}{Fore.BLACK}'
+              f'salary{Style.RESET_ALL} in the following format: 99.99')
+        print(f'or select option "{Back.YELLOW}{Fore.BLACK} C '
+              f'{Style.RESET_ALL}" to calculate it.')
+        user_input = input('If you want to quit You can select option '
+                           f'"{Back.YELLOW}{Fore.BLACK} Q '
+                           f'{Style.RESET_ALL}".\n')
+        if user_input.upper() == "Q":
+            if quit_all() is False:
                 return False
         elif user_input.upper() == "C":
             result = calculate_salary()
@@ -130,6 +141,7 @@ def request_salary():
         elif validate_salary(user_input):
             return "{:.2f}".format(float(user_input))
             break
+
 
 def calculate_salary():
     """
@@ -145,7 +157,7 @@ def calculate_salary():
         salary = "{:.2f}".format(hourly_rate * working_hours)
         if (hourly_rate < 0) or (working_hours < 0):
             print('Negative value!')
-            raise ValueError 
+            raise ValueError
     except ValueError as e:
         print(f"Invalid data: {e}, please try again. \n")
         calculate_salary()
@@ -154,11 +166,12 @@ def calculate_salary():
 
 def validate_salary(salary):
     """
-    Checks input salary data is the right type (integer ot float) and correct format,
+    Checks input salary data is the right type (integer ot float)
+    and correct format,
     The function returns validated salary value.
 
-    The function rounds the value to the demanded format instead of checking its
-    correctness.
+    The function rounds the value to the demanded format instead of
+    checking its correctness.
     """
     try:
         print('Validate salary runs')
@@ -172,7 +185,7 @@ def validate_salary(salary):
         return False
     print(f"Your salary {salary} was successfully validated ")
     return True
-        
+
 
 def get_age():
     """
@@ -181,9 +194,12 @@ def get_age():
     """
     age = 0
     try:
-        age = input(f'Enter your {Back.YELLOW}{Fore.BLACK}age{Style.RESET_ALL} or select "{Back.YELLOW}{Fore.BLACK}Q{Style.RESET_ALL}" to quit\n')
-        if age.upper() == "Q" :
-            if quit_all() == False:
+        age = input('Enter your '
+                    f'{Back.YELLOW}{Fore.BLACK}age{Style.RESET_ALL} '
+                    f'or select "{Back.YELLOW}{Fore.BLACK} Q {Style.RESET_ALL}" '
+                    'to quit\n')
+        if age.upper() == "Q":
+            if quit_all() is False:
                 return False
         elif int(age) < 16 or int(age) > 120:
             print("The age must be in the range between 16 and 120 years old.")
@@ -196,6 +212,7 @@ def get_age():
         age = get_age()
     return age
 
+
 def is_married():
     """
     Get information about formal married from the user.
@@ -207,7 +224,13 @@ def is_married():
     """
     print("Are you living in a formal marriage?")
     while True:
-        married = input(f'Press "{Back.YELLOW}{Fore.BLACK} Y {Style.RESET_ALL}" for Yes or "{Back.YELLOW}{Fore.BLACK} N {Style.RESET_ALL}" if you are not or select Q to quit.\n')
+        married = input('Press "'
+                        f'{Back.YELLOW}{Fore.BLACK} Y {Style.RESET_ALL}" '
+                        'for Yes or '
+                        f'"{Back.YELLOW}{Fore.BLACK} N {Style.RESET_ALL}" '
+                        'if you are not or select '
+                        f'"{Back.YELLOW}{Fore.BLACK} Q {Style.RESET_ALL}" '
+                        'to quit.\n')
         if married[0] == "Y" or married[0] == "y":
             print("You are married")
             return True
@@ -215,56 +238,63 @@ def is_married():
             print("You are not married")
             return False
         elif married.upper() == "Q":
-            if quit_all() == False:
+            if quit_all() is False:
                 return 'quit'
         else:
             print("Invalid value. Please try again!")
 
+
 class Person:
     """
-    Store users data; such as name, age, information about formal relations and 
+    Store users data; such as name, age, information about formal relations and
     users salary.
-    These data are base to further calculations 
+    These data are base to further calculations
     """
     taxes = 0
+
     def __init__(self, name, age, married, salary):
         # This constructor doesn't have taxes parameter because it is used only
-        # for taking data from the user for calculating taxes in the next steps.
+        # for taking data from the user for calculating taxes in the next step.
         self.name = name
         self.age = age
         self.married = married
         self.salary = salary
         self.taxes
-    
+
 
 def create_person():
     """
-    An unfixed bug. Quit option inside each of data request functions interrupt only
-    the functions they are in, but they don't interrupt create_person function.
+    An unfixed bug. Quit option inside each of data request
+    functions interrupt only the functions they are in,
+    but they don't interrupt create_person function.
     After the function is quit create_person function is resumed.
 
-    I'm propably going to have to modify this function and close every nested functions
-    in a loop. Each function has to call the next one in the queue. 
-    Quit option has to call the first function in the queue. 
+    I'm propably going to have to modify this function
+    and close every nested functionsin a loop.
+    Each function has to call the next one in the queue.
+    Quit option has to call the first function in the queue.
     Go up function will call the previous function in the queue.
     """
     flag = True
     print("\nThe application needs some information about you.")
-    while(flag == True):
+    while(flag is True):
         clear()
         name = get_user_name()
         time.sleep(1)
         clear()
         salary = request_salary()
-        if salary == False: continue
+        if salary is False:
+            continue
         time.sleep(1)
         clear()
         age = get_age()
-        if age == False: continue
+        if age is False:
+            continue
         time.sleep(1)
         clear()
         married = is_married()
-        if married == 'quit': continue
+        if married == 'quit':
+            continue
         time.sleep(1)
         clear()
         flag = False
@@ -288,7 +318,8 @@ def calculate_final_tax(salary, partnership):
     prsi = float(calculate_prsi(sal))
     final_tax = (base_tax - tax_credit) + prsi + usc
     rounded_tax = "{:.2f}".format(float(final_tax))
-    print(f"Base tax: {base_tax} Tax Credit: {tax_credit} USC: {usc} PRSI: {prsi} Final Tax: {rounded_tax}")
+    print(f'Base tax: {base_tax} Tax Credit: {tax_credit} '
+          'USC: {usc} PRSI: {prsi} Final Tax: {rounded_tax}')
     if final_tax > 0:
         return rounded_tax
     else:
@@ -300,11 +331,13 @@ def calculate_tax_credit(partnership):
     """
     Tax credits are used to reduce the amount of tax you pay.
     The Personal Tax Credit you get depends on whether you are:
-    single, married or in a civil partnership, widowed or a surviving civil partner,
-    separated, divorced or a former civil partner.
-    Base tax credit for any person is 1700, for a single person it is increase by 1700, 
+    single, married or in a civil partnership, widowed or a
+    surviving civil partner,separated, divorced or a former
+    civil partner.
+    Base tax credit for any person is 1700,
+    for a single person it is increase by 1700,
     for a married it is increased by 3400.
-    The result has to be divided by the number of weeks in a year
+    The result has to be divided by the number of weeks in a year.
     """
     result = 1700
     if(partnership):
@@ -317,7 +350,7 @@ def calculate_tax_credit(partnership):
 
 def calculate_prsi(income):
     """
-    Calculate one-sixth of your earnings over €352.01. 
+    Calculate one-sixth of your earnings over €352.01.
     €377- €352.01 = €24.99. Divided by 6 = €4.17.
     Subtract this from the maximum credit of €12, giving you a credit of €7.83.
     The basic PRSI charge is 4% of €377 = €15.08.
@@ -368,7 +401,7 @@ def calculate_usc(income):
 
 def submit_data(person):
     """
-    Dispalys users data and ask for submition the data 
+    Dispalys users data and ask for submition the data
     and to pass them for the further precessing
     or to not to submit and start the getting information again.
     """
@@ -376,7 +409,10 @@ def submit_data(person):
     print(f'\t{person.age} years old,')
     print(f'\tMarried: {person.married},')
     print(f'\tSalary: {person.salary}.')
-    submit = input(f'Enter "{Back.YELLOW}{Fore.BLACK} Y {Style.RESET_ALL}" to submit your data or select "{Back.YELLOW}{Fore.BLACK} N {Style.RESET_ALL}" to discard data.\n')
+    submit = input(f'Enter "{Back.YELLOW}{Fore.BLACK} Y {Style.RESET_ALL}" '
+                    'to submit your data or select '
+                    f'"{Back.YELLOW}{Fore.BLACK} N {Style.RESET_ALL}" '
+                    'to discard data.\n')
     while True:
         if submit.upper() == "Y":
             # Returning false runs a new iteration in the function_manager loop
@@ -402,7 +438,8 @@ def functions_manager():
     person.taxes = calculate_final_tax(person.salary, person.married)
     update_sheet(person)
     print('Application complete!!!')
-    renew = input(f'\nWould You like to make a new calculation? {Back.YELLOW}{Fore.BLACK}Y/N{Style.RESET_ALL}\n')
+    renew = input('\nWould You like to make a new calculation? '
+                  f'{Back.YELLOW}{Fore.BLACK}Y/N{Style.RESET_ALL}\n')
     while True:
         if renew.upper() == "Y":
             functions_manager()
@@ -412,13 +449,13 @@ def functions_manager():
             print("\n\tThank You. Have a nice day!\n")
             break
         else:
-            renew = input(f'Enter "{Back.YELLOW}{Fore.BLACK} Y {Style.RESET_ALL}" to start a new Calculation or "{Back.YELLOW}{Fore.BLACK} N {Style.RESET_ALL}" to quit.\n')
+            print(f'Enter "{Back.YELLOW}{Fore.BLACK} Y {Style.RESET_ALL}" '
+                  'to start a new Calculation')
+            renew = input(f'or "{Back.YELLOW}{Fore.BLACK} N {Style.RESET_ALL}"'
+                          ' to quit.\n')
 
 
 def main():
     functions_manager()
 
 main()
-
-# Remember to prolong sleep time
-
